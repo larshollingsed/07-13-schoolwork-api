7b098b3023dbc5b6797378776c35c30ecfe44694
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-13 13:56:35 -0500

    Add assignment works - still needs to add to bridge table

--- controllers/assignment_controller.rb
+++ controllers/assignment_controller.rb
@@ -1,3 +1,14 @@
 get "/" do
   erb :"home"
 end
+
+get "/add_assignment_form" do
+  @collaborators = Collaborator.all
+  erb :"/assignments/add_assignment_form"
+end
+
+get "/add_assignment_confirm" do
+  @new_assignment = Assignment.add({"assignment_name" => params["assignment"]["name"], "link" => params["assignment"]["link"], "repository" => params["assignment"]["repository"], "description" => params["assignment"]["description"]})
+  erb :"home"
+end
+  
