dbf32497b7cebf05cfb2e541b0d19bfc48897313
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-13 13:16:38 -0500

    See all collaborators functional

--- controllers/collaborator_controller.rb
+++ controllers/collaborator_controller.rb
@@ -6,3 +6,8 @@ get "/add_collaborator_confirm" do
   @new_collaborator = Collaborator.add({"collaborator_name" => params["collaborator"]["name"]})
   erb :"/home"
 end
+
+get "/see_all_collaborators" do
+  @collaborators = Collaborator.all
+  erb :"/collaborators/see_all_collaborators"
+end
