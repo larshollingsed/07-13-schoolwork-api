9eccb01922461f5b1dafe9884312197e14a7568f
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-14 11:35:15 -0500

    Added see all assignments with AJAX

--- public/global.js
+++ public/global.js
@@ -1,22 +1,23 @@
-var req = new XMLHttpRequest();
-
 function seeAllAssignments(event) {
-  // event.preventDefault();
+  event.preventDefault();
+  
+  var req = new XMLHttpRequest();
   req.open("get", "/api/assignments");
-
+  
   req.addEventListener("load", function(){
-  for(var i = 0; i < this.response.length; i++) {
-    var newDivName = "assignment" + this.response[i].id;
-    var newDiv = document.createElement(newDivName);
-    newDiv.innerHtml = this.response[i].assignment_name;
-    
-    var currentDiv = document.getElementById("main");
-    document.body.insertBefore(newDiv, currentDiv.nextSibling);
-  }
-})
-req.responseType = "json";
-req.send();
+    for(var i = 0; i < this.response.length; i++) {
+      var newDivId = "assignment" + this.response[i].id;
+      var newDiv = document.createElement("p");
+      newDiv.id = newDivId
+      newDiv.innerText = this.response[i].assignment_name;
+      var currentDiv = document.getElementById("assignments");
+      currentDiv.parentNode.insertBefore(newDiv, currentDiv.lastSibling);
+      
+      
+      // document.body.insertBefore(newDiv, currentDiv.lastChild);
+    }
+  });
+  req.responseType = "json";
+  req.send();
 }
-
-
-document.getElementById("see_all_assignments").onclick = seeAllAssignments();
+document.getElementById("see_all_assignments").onclick = seeAllAssignments;
