6c7e0f262b1f8fc1d6d14538b571e66ecadac415
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-13 20:14:01 -0500

    Add and delete collaborator API

--- controllers/api_controller.rb
+++ controllers/api_controller.rb
@@ -1,3 +1,4 @@
+# see all assignments
 get "/api/assignments" do
   assignments = Assignment.all
   json_array = []
@@ -7,6 +8,8 @@ get "/api/assignments" do
   json json_array
 end
 
+# see all assignments a collaborator worked on
+# :id is the collaborator's id
 get "/api/assignments/:id" do
   assignment = Assignment.find(params["id"])
   collaborator_array = assignment.find_collaborators
@@ -19,6 +22,7 @@ get "/api/assignments/:id" do
   json json_array  
 end
 
+# see all collaborators
 get "/api/collaborators" do
   collaborators = Collaborator.all
   json_array = []
@@ -28,6 +32,8 @@ get "/api/collaborators" do
   json json_array
 end
 
+# get all assignments a collaborator has worked on
+# :id is the collaborator's id
 get "/api/collaborators/:id" do
   collaborator = Collaborator.find(params["id"])
   assignment_ids = collaborator.get_assignments
@@ -40,15 +46,33 @@ get "/api/collaborators/:id" do
 end
 
 # not working
-get "/api/assignments/new/:name/:github" do
-  assignment = Assignment.add({"assignment_name" => params["name"], "repository" => params["github"]})
-  json_array = []
-  json_array << assignment.json_format
-  json json_array
-end
+# get "/api/assignments/new/:name/:github" do
+#   assignment = Assignment.add({"assignment_name" => params["name"], "repository" => params["github"]})
+#   json_array = []
+#   json_array << assignment.json_format
+#   json json_array
+# end
 
+# delete an assignment
+# :id is the assignment's id
 get "/api/assignments/delete/:id" do
   assignment = Assignment.find(params["id"].to_i)
   assignment.delete
   assignment.delete_collaborations
+  json assignment.json_format
+end
+
+# add a collaborator
+# :name is the collaborator's name
+get "/api/collaborators/add/:name" do
+  collaborator = Collaborator.add({"collaborator_name" => params["name"]})
+  json collaborator.json_format
+end
+
+# delete a collaborator
+# :id is the collaborator's id
+get "/api/collaborators/delete/:id" do
+  collaborator = Collaborator.find(params["id"].to_i)
+  collaborator.delete
+  json collaborator.json_format
 end
